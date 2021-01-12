90f21dc494e20b43dcf7767537d5a5d93028c7b8
haru <seizou.genkei@protonmail.com>
2021-01-12 10:07:22 -0700

    dataset: Added prose, removed 1stperson

--- dataset/compile.sh
+++ dataset/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 python3 build_data.py --data_dir=2ndperson/ --output="data_2ndperson.txt"
-python3 build_data.py --data_dir=1stperson/ --output="data_1stperson.txt" --first_to_second
+python3 build_data.py --data_dir=prose/ --output="data_prose.txt"
 python3 build_data.py --output="data_compiled.txt" --combine_output_data
